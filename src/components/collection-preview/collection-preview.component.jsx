b16f61490d05568ccee2a175c77c4cbbea0caa98
KillerCoder <54954227+KillerCoderPT@users.noreply.github.com>
2021-02-03 03:06:28 +0000

    Added the option to click on the Sections Headers and go to the page

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,20 +1,25 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import CollectionItem from "../collection-item/collection-item.component";
+
 import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({ title, items }) => {
-  return (
-    <div className="collection-preview">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <div className="preview">
-        {items
-          .filter((item, idx) => idx < 4)
-          .map((item) => (
-            <CollectionItem key={item.id} item={item} />
-          ))}
-      </div>
+const CollectionPreview = ({ title, items, history, match }) => (
+  <div className="collection-preview">
+    <div className="title">
+      <h1 onClick={() => history.push(`${match.url}/${title.toLowerCase()}`)}>
+        {title.toUpperCase()}
+      </h1>
     </div>
-  );
-};
+    <div className="preview">
+      {items
+        .filter((item, idx) => idx < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+    </div>
+  </div>
+);
 
-export default CollectionPreview;
+export default withRouter(CollectionPreview);
