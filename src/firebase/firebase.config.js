c28ce9dc7b451740bd9bb92f043c77c228255a04
KillerCoder <54954227+KillerCoderPT@users.noreply.github.com>
2021-02-03 00:46:14 +0000

    Changed process.env names

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,8 +1,8 @@
 export const FirebaseConfig = {
-  apiKey: process.env.REACT_APIKEY,
-  authDomain: process.env.REACT_AUTHDOMAIN,
-  projectId: process.env.REACT_PROJECTID,
-  storageBucket: process.env.REACT_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_MESSAGINGSENDERID,
-  appId: process.env.REACT_APPID,
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
