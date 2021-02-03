3168a176d10f83238994bf8b13148d374fea7739
KillerCoder <54954227+KillerCoderPT@users.noreply.github.com>
2021-02-03 00:37:28 +0000

    Added process.env calls from heroku to the firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,8 +1,8 @@
 export const FirebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: "",
+  apiKey: process.env.REACT_APIKEY,
+  authDomain: process.env.REACT_AUTHDOMAIN,
+  projectId: process.env.REACT_PROJECTID,
+  storageBucket: process.env.REACT_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_MESSAGINGSENDERID,
+  appId: process.env.REACT_APPID,
 };
